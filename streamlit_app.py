025011431fb7a1a79383eb142018eb2ca08ff73b
mkkubrick <109222614+mkkubrick@users.noreply.github.com>
2023-02-20 15:34:00 +0000

    Update streamlit_app.py
    fixed function typo and added header

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,16 +37,16 @@ try:
 except URLError as e:
   streamlit.error()
 
-
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 #my_data_rows = my_cur.fetchall()
-#streamlit.header("The fruit load list contains")
+#streamlit.header("The fruit load list contains:")
 #streamlit.dataframe(my_data_rows)
 
+streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
-def get_fruit_load_lsit():
+def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
     return my_cur.fetchall()
